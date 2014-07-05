32986b7062bad6308baa02afcaf8b41c97ad3e68
SavinVarshney <savin.varshney@gmail.com>
2014-07-06 01:40:24 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mheal::Application.config.secret_token = '8bed76702c35a69276923c68ab8ed7fd29f48694d2b6c250845a79a73894bad081153d92a95c6d0d3cb6998d5d04fd15d1f169dd8a3e119094fd011954199662'
